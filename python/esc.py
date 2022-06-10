2e7ef33dd92c190773121e692ac0a6e6ed325ffe
Donie Kelly <donie.kelly@gmail.com>
2022-06-10 19:54:51 +0100

    Updated comments

--- python/esc.py
+++ python/esc.py
@@ -1,35 +1,45 @@
 from time import sleep
 from machine import Pin
 from machine import PWM
-import sys
-import os
 
+# Use pin #0 for ESC controller 1
+# Use pin #1 for ESC controller 2
 Pin(0, Pin.OUT)
-pwm = PWM(Pin(0))
-pwm.freq(50)
+Pin(1, Pin.OUT)
 
-#Function to set an angle
-#The position is expected as a parameter
+pwm1 = PWM(Pin(0))
+pwm2 = PWM(Pin(1))
 
+pwm1.freq(50) # Freq must be 50Hz for both controllers
+pwm1.freq(50)
+
+# map function
 def map(x, in_min, in_max, out_min, out_max):
     return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
+# Set speeds to be same on both ESC controllers
+# Map the value from 0-100% to a pulse from 1ms to 2ms
 def speed(speed):
     speed = map(speed, 0,100, 1000,2000)
-    pwm.duty_ns(speed * 1000) # in nanoseconds
+    pwm1.duty_ns(speed * 1000) # in nanoseconds
+    pwm2.duty_ns(speed * 1000) # in nanoseconds
     
+# Turn off the outputs
+def escOff():
+    pwm1.duty_ns(0)
+    pwm2.duty_ns(0)
+
+# Arm the ESC controllers, not sure if this is correct yet as it depends on 
 def arm():
-    print("UP")
-    speed(100)
-    sleep(2)
-    print("DOWN")
+    print("Arming")
     speed(0)
     sleep(2)
-    print("READY")
+    print("Ready")
 
+# trigger the arming
 arm()
 
+# TODO: Need to add joystick and servo for steering and attach trottles to stick
 while True:
-    
     speed(0)
     sleep(1)
