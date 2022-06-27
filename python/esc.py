b6007981bad30bd74f259f2325ab708b5a7e8055
Donie Kelly <donie.kelly@gmail.com>
2022-06-27 22:24:06 +0100

    Updated comment for speed controller

--- python/esc.py
+++ python/esc.py
@@ -45,7 +45,7 @@ def map(x, in_min, in_max, out_min, out_max):
 
 # Set speeds to be same on both ESC controllers
 # Map the value from 0-100% to a pulse from 1100ms to 1900ms for high speed
-# Map the value from 0-100% to a pulse from 1400ms to 1600ms for high speed
+# Map the value from 0-100% to a pulse from 1400ms to 1600ms for low speed
 def speed(speed):
     if speedProfile == 1:
         speed = map(speed, 0,100, 1100,1900) #ESC
