0677a9737acd25f67c4244a646e5919dc0e41f65
Donie Kelly <donie.kelly@gmail.com>
2023-07-28 23:05:01 +0100

    Fixed joystick mapping code

--- python/main.py
+++ python/main.py
@@ -7,33 +7,30 @@ import thread_i2c
 import _thread
 
 # init
-i2c = thread_i2c.i2c()
-display = oleddisplay.OLED(i2c = i2c)
-joystick = joystick.Joystick(i2c = i2c)
+i2c = thread_i2c.ThreadSafeI2C()
+display = oleddisplay.OLED(i2c=i2c)
+joystick = joystick.Joystick(i2c=i2c)
 
 # Init MCP23009 to control buttons and lights
-controls = Controls.mcp23009(i2c = i2c)
+controls = Controls.mcp23009(i2c=i2c)
 controls.setupIO()
 t200 = truster.PowerManager(display)
 
-
 def timeCrit():
-
     print("Starting...")
     while True:
         axis = joystick.readAxis()
-        
+        pcm = joystick.steering(axis)
         if not controls.isSafetyOn():
             t200.killTrusters()
         else:
-            t200.calculateTrusterPCM(axis)
-        
+            t200.setTrusterPCM(pcm)
+
         if controls.isJoystickHatOn():
-           print("HAT")
+            print("HAT")
         sleep(0.1)
 
 def displayUpdates():
-
     while True:
         controls.readGPIO()
         display.updateBattery()
@@ -50,10 +47,7 @@ def displayUpdates():
 
         sleep(0.05)
 
-
+# Run display on one thread and main control loop on other
 if __name__ == "__main__":
-
     _thread.start_new_thread(displayUpdates, ())
     timeCrit()
-
-
